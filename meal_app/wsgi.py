29df2971043b2332b05de44dc215a14c83ac3f55
Chris Lowe <csl619@outlook.com>
2021-07-13 16:02:11 +0100

    add base code for allowing use of .env file for environment variables

--- meal_app/wsgi.py
+++ meal_app/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meal_app.settings')
 
