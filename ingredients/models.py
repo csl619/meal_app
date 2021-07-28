868ee6b35ac8acf4536862ff4fe2762966966db9
Chris Lowe <csl619@outlook.com>
2021-07-28 16:33:26 +0100

    add default unit type to ingredients

--- ingredients/models.py
+++ ingredients/models.py
@@ -3,6 +3,19 @@ from django.contrib.auth.models import User
 
 
 class Ingredient(models.Model):
+    unit_code = [
+        ('1', 'item'),
+        ('2', 'millilitre'),
+        ('3', 'pint'),
+        ('4', 'teaspoon'),
+        ('5', 'tablespoon'),
+        ('6', 'gram'),
+        ('7', 'ounce'),
+        ('8', 'piece'),
+        ('9', 'pinch'),
+        ('10', 'cup'),
+    ]
+
     name = models.CharField(
         verbose_name='Name', max_length=100, blank=False)
     date_added = models.DateField(
@@ -10,6 +23,8 @@ class Ingredient(models.Model):
     related_user = models.ForeignKey(
         User, blank=True, null=True, default=None,
         on_delete=models.CASCADE, related_name='+')
+    default_unit = models.CharField(
+        max_length=2, choices=unit_code, default='1')
 
     def __str__(self):
         return str(f'{self.name}')
