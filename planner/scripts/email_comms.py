cdea4e869f3aa1147e82898afa4d186f81f68344
Chris Lowe <csl619@outlook.com>
2021-08-02 13:41:36 +0100

    create email class for processing emails moving forward

--- planner/scripts/email_comms.py
+++ planner/scripts/email_comms.py
@@ -1,50 +1,60 @@
 from django.core.mail import EmailMultiAlternatives, get_connection
 from django.utils.html import strip_tags
 from django.template.loader import render_to_string
-from os import getenv
-
-from planner.models import Week
 
 
-class send_email():
-    def __init__(self, item, files, user):
-        self.item = item
-        self.files = files
-        self.user = user
+class Email():
+    def __init__(
+            self, host, port, admin_email, password, use_tls, debug=True):
+        self.host = host
+        self.port = port
+        self.admin = admin_email
+        self.password = password
+        self.use_tls = use_tls
+        self.debug = debug
 
-    def process_email(self, s, c, files, e):
+    def __get_connection(self):
         connect = get_connection(
-            host=getenv("ADMIN_HOST"),
-            port=getenv("ADMIN_PORT"),
-            username=getenv("ADMIN_EMAIL"),
-            password=getenv("ADMIN_PASSWORD"),
-            use_tls=getenv("ADMIN_TLS")
+            host=self.host,
+            port=self.port,
+            username=self.admin,
+            password=self.password,
+            use_tls=self.use_tls
         )
-        with connect as connection:
-            msg = EmailMultiAlternatives(
-                s, c['text_content'], getenv("ADMIN_EMAIL"), e,
-                connection=connection)
-            msg.attach_alternative(c['html_content'], "text/html")
-            msg.content_subtype = "text/html"
-            for file in files:
-                msg.attach(file['name'], file['file'], file['type'])
-            msg.send()
-            return 'Email Message Sent'
+        return connect
 
-    def week_planner_email(self):
-        week_item = Week.objects.filter(id=self.item).first()
-        subject = f'{week_item.name} - Planned Meals'
-        context = {
-            'id': week_item.id,
-            'user': self.user,
-            'site': getenv('SITE_URL'),
-        }
-        content = {}
-        content['html_content'] = render_to_string(
-            'email_comms/week_planner_email.html', context=context)
-        content['text_content'] = strip_tags(content['html_content'])
-        if getenv("DEBUG_TYPE") == 'True':
-            e = [getenv("ADMIN_EMAIL")]
-        else:
-            e = [self.user.email]
-        self.process_email(subject, content, self.files, e)
+    def attachments(self, files):
+        self.attachments = files
+
+    def recipients(self, emails):
+        if self.debug:
+            emails = [self.admin]
+        self.recipients = emails
+
+    def sender(self, email):
+        if self.debug:
+            email = self.admin
+        self.sender = email
+
+    def subject(self, subject):
+        self.subject = subject
+
+    def body(self, template, context_vars=None):
+        context = {}
+        if context_vars:
+            for item in context_vars:
+                context[item] = context_vars[item]
+        self.html_content = render_to_string(template, context=context)
+        self.text_content = strip_tags(self.html_content)
+
+    def send(self):
+        with self.__get_connection() as connection:
+            msg = EmailMultiAlternatives(
+                self.subject, self.text_content, self.sender, self.recipients,
+                connection=connection)
+            msg.attach_alternative(self.html_content, "text/html")
+            msg.content_subtype = "text/html"
+            if self.attachments:
+                for file in self.attachments:
+                    msg.attach(file['name'], file['file'], file['type'])
+            return msg.send()
