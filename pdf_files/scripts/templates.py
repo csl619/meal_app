1f1f77b0401531f9619826a12e73d63d5ba66a20
Chris Lowe <csl619@outlook.com>
2021-08-02 13:42:21 +0100

    create class for generating pdf files

--- pdf_files/scripts/templates.py
+++ pdf_files/scripts/templates.py
@@ -1,80 +1,46 @@
-from django.shortcuts import get_object_or_404
 from django.template.loader import render_to_string
-from django.db.models import Sum, Avg
-from django.db.models.functions import Coalesce
 from django.http import HttpResponse
-from django.utils.text import slugify
 from weasyprint import HTML, CSS
 from weasyprint.fonts import FontConfiguration
 
-from decimal import Decimal, ROUND_DOWN
-from os import getenv
-from datetime import datetime
 
-from meals.models import (Meal, MealIngredient)
-from planner.models import (Week, WeekDays, WeekIngredient)
+class Pdf():
+    def __init__(self, site_url, directory, browser_output=False):
+        self.browser_output = browser_output
+        self.data = {
+            'site_url': site_url,
+            'directory': directory,
+            'browser_output': browser_output
+        }
 
+    def template(self, temp_vars):
+        self.template = temp_vars['template']
+        self.css = temp_vars['css']
 
-def meal_pdf(request, id, res_type):
-    meal = Meal.objects.filter(pk=id).first()
-    ingredients = MealIngredient.objects.filter(
-        meal_id=meal).order_by('related_ingredient__name')
-    response = HttpResponse(content_type="application/pdf")
-    response['Content-Disposition'] = (
-        "inline; filename={name}-{type}.pdf".format(
-            name=slugify(meal.name).replace('-', '_'),
-            type=slugify('meal_card')
-        ))
-    template = 'meals/meal_pdf.html'
-    css_file = 'staticfiles/site/css/meal_pdf.css'
-    d = {
-        'meal': meal,
-        'ingredients': ingredients,
-        'site_url': getenv("SITE_URL")}
-    html = render_to_string(template, d)
-    if res_type == 'pdf':
-        response = HTML(
-            string=html, base_url=request.build_absolute_uri()).write_pdf(
-                font_config=FontConfiguration(), stylesheets=[CSS(css_file)]
-                )
-    else:
-        HTML(
-            string=html, base_url=request.build_absolute_uri()).write_pdf(
+    def context_data(self, data):
+        for item in data:
+            self.data[item] = data[item]
+
+    def name(self, file_name):
+        self.file_name = file_name
+
+    def generate(self, request=None):
+        if self.browser_output:
+            response = HTML(
+                string=render_to_string(self.template, self.data),
+                base_url='').write_pdf(
+                    font_config=FontConfiguration(),
+                    stylesheets=[CSS(self.css)]
+                    )
+        else:
+            response = HttpResponse(content_type="application/pdf")
+            response['Content-Disposition'] = (
+                f"inline; filename={self.file_name}.pdf")
+            HTML(
+                string=render_to_string(self.template, self.data),
+                base_url=request.build_absolute_uri()
+            ).write_pdf(
                 response, font_config=FontConfiguration(),
-                stylesheets=[CSS(css_file)]
+                stylesheets=[CSS(self.css)]
                 )
-    return response
-
-
-def week_pdf(request, id, res_type):
-    week = Week.objects.filter(pk=id).first()
-    ingredients = WeekIngredient.objects.filter(
-        week_id=week).order_by('related_ingredient__name')
-    meals = WeekDays.objects.filter(related_week=week).order_by('meal_date')
-    response = HttpResponse(content_type="application/pdf")
-    response['Content-Disposition'] = (
-        "inline; filename={name}-{type}.pdf".format(
-            name=slugify(
-                f'we_{week.week_end.strftime("%d-%m-%Y")}').replace('-', '_'),
-            type=slugify('planned_meals')
-        ))
-    template = 'planner/week_pdf.html'
-    css_file = 'staticfiles/site/css/week_pdf.css'
-    d = {
-        'week': week,
-        'meals': meals,
-        'ingredients': ingredients,
-        'site_url': getenv("SITE_URL")}
-    html = render_to_string(template, d)
-    if res_type == 'pdf':
-        response = HTML(
-            string=html, base_url=request.build_absolute_uri()).write_pdf(
-                font_config=FontConfiguration(), stylesheets=[CSS(css_file)]
-                )
-    else:
-        HTML(
-            string=html, base_url=request.build_absolute_uri()).write_pdf(
-                response, font_config=FontConfiguration(),
-                stylesheets=[CSS(css_file)]
-                )
-    return response
+        return response
